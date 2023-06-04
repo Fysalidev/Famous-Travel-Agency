dd87fd6ac57c524742f21c76d34138a783a8e39c
ROBILLIARD Grégory <gregory.robilliard@icloud.com>
2023-06-04 16:49:23 +0200

    Try to fix trouble

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import { Nunito } from "next/font/google";
+
+import Navbar from "@/app/components/Navbar/Test";
+import RegisterModal from "./components/modals/RegisterModal";
+
+import ToasterProvider from "./providers/ToasterProvider";
+
+import "./globals.css";
+
+const font = Nunito({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Airbnb",
+  description: "Airbnb clone built with Next.js and Tailwind CSS",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ToasterProvider />
+        <RegisterModal />
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
+
+// Last try
